ef2ca38657e0706a8c91c846a6ae1fcb760022e4
guidostark37 <bguido10@gmail.com>
2023-11-15 16:10:58 -0500

    terminando

--- app/src/main/java/com/example/who_wants_to_be_a_millionaire/Home.kt
+++ app/src/main/java/com/example/who_wants_to_be_a_millionaire/Home.kt
@@ -1,20 +1,74 @@
 package com.example.who_wants_to_be_a_millionaire
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.who_wants_to_be_a_millionaire.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
+    var punto:Int =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.apply {
-            btnplay.setOnClickListener {
-                startActivity(Intent(this@Home,Preguntas::class.java))
+
+        var time = 10
+        var record = 0
+        var timer = object : CountDownTimer(11000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+                --time
+                var guardar = getSharedPreferences("respuesta", Context.MODE_PRIVATE).edit()
+                var prueba = getSharedPreferences("respuesta", Context.MODE_PRIVATE).getString("premio", "")
+                if (prueba!=""){
+                    punto+=1
+                }
+
+                if (punto>0){
+                    binding.muestra10.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>1){
+                    binding.muestra9.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>2){
+                    binding.muestra9.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>3){
+                    binding.muestra8.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>4){
+                    binding.muestra7.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>5){
+                    binding.muestra6.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>6){
+                    binding.muestra5.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>7){
+                    binding.muestra9.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>8){
+                    binding.muestra9.setBackgroundResource(R.drawable.correcto)
+                }else if (punto>9){
+                    binding.muestra9.setBackgroundResource(R.drawable.correcto)
+                }else{
+                    Toast.makeText(this@Home, "Tu puntuacion es: ${punto}", Toast.LENGTH_SHORT).show()
+                }
+
             }
+
+
+            override fun onFinish() {
+
+                time = 10
+                startActivity(Intent(this@Home, Preguntas::class.java))
+
+
+
+
+
+            }
+
         }
+        timer.start()
+
     }
 }
--- app/src/main/java/com/example/who_wants_to_be_a_millionaire/Preguntas.kt
+++ app/src/main/java/com/example/who_wants_to_be_a_millionaire/Preguntas.kt
@@ -1,8 +1,11 @@
 package com.example.who_wants_to_be_a_millionaire
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import com.example.who_wants_to_be_a_millionaire.Model.datos
 import com.example.who_wants_to_be_a_millionaire.databinding.ActivityPreguntasBinding
@@ -19,38 +22,52 @@ class Preguntas : AppCompatActivity() {
     lateinit var binding: ActivityPreguntasBinding
     lateinit var db:FirebaseDatabase
     lateinit var dataref:DatabaseReference
+    var list:MutableList<datos> = mutableListOf()
+    var respuesta:String = ""
+
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPreguntasBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var time=10
+
+
         db = FirebaseDatabase.getInstance()
         dataref =db.getReference("datos")
 
         traer()
+        var timer=object: CountDownTimer(11000,1000){
 
-        binding.apply {
-            txtopcion1.setOnClickListener {
-                txtopcion1.setBackgroundResource(R.drawable.correcto)
-                txtopcion1.setTextColor(R.color.black)
-            }
-            txtopcion2.setOnClickListener {
-                txtopcion2.setBackgroundResource(R.drawable.incorrecto)
-                txtopcion2.setTextColor(R.color.black)
-            }
-            txtopcion3.setOnClickListener {
-                txtopcion3.setBackgroundResource(R.drawable.seleccionado)
-                txtopcion3.setTextColor(R.color.black)
-            }
-            txtopcion4.setOnClickListener {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.txttiempo.text = time.toString()
+                --time
+
 
             }
+
+            override fun onFinish() {
+
+                time=10
+
+                list.clear()
+                startActivity(Intent(this@Preguntas,Home::class.java))
+
+
+            }
+
         }
+        timer.start()
+
+
     }
+
     fun traer(){
+
         dataref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                var list:MutableList<datos> = mutableListOf()
+
 
               snapshot.children.forEach {
                   var dato=it.getValue(datos::class.java)
@@ -58,15 +75,63 @@ class Preguntas : AppCompatActivity() {
                   list.add(datos(dato!!.pregunta,dato!!.respuesta,dato!!.opcion1,dato!!.opcion2,dato!!.opcion3,dato!!.opcion4))
 
               }
-               var num = Random.nextInt(0,9)
-
+                var num = Random.nextInt(0,9)
+                var guardar = getSharedPreferences("respuesta", Context.MODE_PRIVATE).edit()
                 binding.apply {
                     txtpregunta.text =list[num].pregunta
                     txtopcion1.text = list[num].opcion1
                     txtopcion2.text = list[num].opcion2
                     txtopcion3.text = list[num].opcion3
                     txtopcion4.text = list[num].opcion4
+                    respuesta = list[num].respuesta
+
+                    txtopcion1.setOnClickListener {
+                        txtopcion1.setBackgroundResource(R.drawable.seleccionado)
+                        if (txtopcion1.text ==respuesta){
+                            guardar.putString("premio","gano").commit()
+                            txtopcion1.setBackgroundResource(R.drawable.correcto)
+                        }else{
+                            txtopcion1.setBackgroundResource(R.drawable.incorrecto)
+                            guardar.putString("premio","").commit()
+                        }
+                    }
+
+
+                    txtopcion2.setOnClickListener {
+                        txtopcion2.setBackgroundResource(R.drawable.seleccionado)
+                        if (txtopcion2.text ==respuesta){
+                            guardar.putString("premio",1.toString()).commit()
+                            txtopcion2.setBackgroundResource(R.drawable.correcto)
+                        }else{
+                            txtopcion2.setBackgroundResource(R.drawable.incorrecto)
+                            guardar.putString("premio","").commit()
+                        }
+                    }
+
+                    txtopcion3.setOnClickListener {
+                        txtopcion3.setBackgroundResource(R.drawable.seleccionado)
+                        if (txtopcion3.text ==respuesta){
+                            guardar.putString("premio",1.toString()).commit()
+                            txtopcion3.setBackgroundResource(R.drawable.correcto)
+                        }else{
+                            txtopcion3.setBackgroundResource(R.drawable.incorrecto)
+                            guardar.putString("premio","").commit()
+                        }
+                    }
+
+                    txtopcion4.setOnClickListener {
+                        txtopcion4.setBackgroundResource(R.drawable.seleccionado)
+                        if (txtopcion4.text ==respuesta){
+                            guardar.putString("premio",1.toString()).commit()
+                            txtopcion4.setBackgroundResource(R.drawable.correcto)
+                        }else{
+                            txtopcion4.setBackgroundResource(R.drawable.incorrecto)
+                            guardar.putString("premio","").commit()
+                        }
+                    }
                 }
+
+
             }
 
             override fun onCancelled(error: DatabaseError) {
